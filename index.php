1ffed38e74b0a5aab33d4cf14d15b1f2b8b2f357
vanesantillana <vanessa.santillana@ucsp.edu.pe>
2017-02-09 03:04:01 -0500

    arrendamientios

--- index.php
+++ index.php
@@ -301,8 +301,6 @@ if (!isset($_POST['email'])) {
 	    </div>
 	    <div class="publicacion">
 			  
-    </form> 
-
 
 		</div>
 		<div class="col-md-12">
@@ -312,8 +310,10 @@ if (!isset($_POST['email'])) {
 		</div>
 		<div class="col-md-12">
 			<h2>Formatos</h2><br>		
-		    <button type="button" class="btn btn-link" data-toggle="modal" data-target="#myModal1">Guía Pagos Varios</button>
-		    <button type="button" class="btn btn-link" data-toggle="modal" data-target="#myModal2">Guía Pago Fácil Nuevo Régimen Único Simplificado</button>
+		    <button type="button" class="btn btn-link" data-toggle="modal" data-target="#myModal1">Guía Pagos Varios</button><br>
+		    <button type="button" class="btn btn-link" data-toggle="modal" data-target="#myModal3">Guía Para Arrendamiento</button><br>
+		    <button type="button" class="btn btn-link" data-toggle="modal" data-target="#myModal2">Guía Pago Fácil Nuevo Régimen Único Simplificado</button><br>
+		    
 		    <!-- Modal Pagos Varios -->
 			<div class="modal fade" id="myModal1" role="dialog">
 			    <div class="modal-dialog modal-lg">
@@ -423,7 +423,7 @@ if (!isset($_POST['email'])) {
 			      
 			    </div>
 			</div>
-			  <!-- Modal Nuevo Regimen -->
+			<!-- Modal Nuevo Regimen -->
 			<div class="modal fade" id="myModal2" role="dialog">
 			    <div class="modal-dialog modal-lg">
 
@@ -503,6 +503,111 @@ if (!isset($_POST['email'])) {
 				              <label ><span class="fa fa-external-link"></span> Ingresa la imagen:</label>
 				              <input type="text" name="codigo" class="form-control"  id="mi-t-fraccionamiento" placeholder="XXXX" >
 				            </div>				  
+				          </div>
+				          <div class="row">
+			                    <div class="col-md-11 col-sm-12 col-xs-12 form-group">
+			                        <input class='navbar-right btn btn-primary' style="color: #FFF; background-color: #0b5394" size="100" type="submit" value="Generar PDF" />			                     
+			                    </div>
+			            	</div>
+					    </form> 
+					  </div>
+			        </div>
+			      </div>
+			      
+			    </div>
+			</div>
+			<!-- Modal Arrendamiento -->
+			<div class="modal fade" id="myModal3" role="dialog">
+			    <div class="modal-dialog modal-lg">
+
+			      <div class="modal-content">
+			        <div class="modal-header" style="background-color:#0b5394">
+			          <button type="button" style="color:#FFFFFF" class="close" data-dismiss="modal">&times;</button>
+			          <h4 class="modal-title" style="color:#FFFFFF" align="center"><strong>Guía Para Arrendamiento</strong></h4>
+			        </div>
+			        <div class="modal-body">
+			          <div>
+			          	<form name="formulario" method="post" action="guias/formato.php" target="_blank">
+					      <div id="row">
+				           <h4><b>Datos Necesarios del Arrendador</b></h4>
+				            <div class="col-md-6 col-sm-12 col-xs-12 form-group">
+				              <label ><span class="fa fa-user"></span> N° de RUC:</label>
+				              <input type="text" name="ruc" class="form-control" id="mi-ruc" placeholder="XXXXXXXXXXX">
+				            </div>
+
+				            <div class="col-md-3 col-sm-12 col-xs-12 form-group">
+				              <label ><span class="fa fa-calendar"></span> Periodo:</label>
+				              <input type="text" name="anio" class="form-control"  id="mi-periodo" placeholder="2017">
+				            </div>
+
+				            <div class="col-md-3 col-sm-12 col-xs-12 form-group">
+				              <label ><span class="fa fa-calendar-o"></span> Mes:</label>
+				              <select class="form-control" name="mes">
+								      <option value="01">Enero</option>
+								      <option value="02">Febrero</option>
+								      <option value="03">Marzo</option>
+								      <option value="04">Abril</option>
+								      <option value="05">Mayo</option>
+								      <option value="06">Junio</option>
+								      <option value="07">Julio</option>
+								      <option value="08">Agosto</option>
+								      <option value="09">Septiembre</option>
+								      <option value="10">Octubre</option>
+								      <option value="11">Noviembre</option>
+								      <option value="12">Diciembre</option>
+								</select>
+				            </div>
+				          </div>
+					      <div id="row">	
+					      		<h4><b>Datos Necesarios del Inquilino</b></h4>					    
+							    <div class="col-md-6 col-sm-12 col-xs-12 form-group">
+					              <label ><span class="fa fa-list-alt"></span> Código del Documento de Indentidad:</label>
+					              <select class="form-control" name="cod">
+									      <option value="6">RUC</option>
+									      <option value="1">DNI o Libreta Electoral</option>
+									      <option value="2">Carné de Fuerzas Policiales</option>
+									      <option value="3">Carné de Fuerzas Armadas</option>
+									      <option value="4">Carné de Extranjería</option>
+									      <option value="7">Pasaporte</option>
+									</select>
+					            </div>
+							    <div class="col-md-6 col-sm-12 col-xs-12 form-group">
+					              <label ><span class="fa fa-envelope-square"></span> Número del Documento de Indentidad:</label>
+					              <input type="text" name="num" class="form-control"  id="mi-importe" placeholder="XXXXXXXX">
+					            </div>	
+														    
+					      </div>
+		
+				          <div id="row">		
+				          	<h4><b>¿Es ésta una declaración recitificatoria / sustitutoria?</b></h4>
+				          	<b> &nbsp;&nbsp;&nbsp;Si marco SI, llene el N° de orden e importe.</b><br>		
+				            <div class="col-md-4 col-sm-12 col-xs-12 form-group">
+				              <label ></label>
+								<input id="declaracion" type="checkbox" name="declaracion" />
+					            <label for=declaracion class=cb-label></label>
+					           	<hr style="visibility: hidden;">
+
+				            </div>
+
+				            <div class="col-md-4 col-sm-12 col-xs-12 form-group">
+				              <label ><span class="fa fa-external-link"></span> Número de orden u Operación:</label>
+				              <input type="text" name="codigo" class="form-control"  id="mi-t-fraccionamiento" placeholder="0" ><br>
+				            </div>
+
+				            <div class="col-md-4 col-sm-12 col-xs-12 form-group">
+				              <label ><span class="fa fa-money"></span> Importe a Pagar:</label>
+				              <input type="text" name="codigo" class="form-control"  id="mi-t-fraccionamiento" placeholder="0" ><br>
+				            </div>				  
+				          </div>
+				        
+				          <div id="row">
+				          	<h4><b>Monto del Alquiler</b></h4>				         	
+				            <div class="col-md-12 col-sm-12 col-xs-12 form-group">
+				              <label ></label>
+								<label ><span class="fa fa-money"></span> Importe:</label>
+				             	<input type="text" name="codigo" class="form-control"  id="mi-t-fraccionamiento" placeholder="0" >
+				            </div>
+				          	
 				          </div>
 				          <div class="row">
 			                    <div class="col-md-11 col-sm-12 col-xs-12 form-group">
