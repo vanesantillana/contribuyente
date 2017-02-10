f014382b25a22628c353978ce0d75492ccfbb323
vanesantillana <vanessa.santillana@ucsp.edu.pe>
2017-02-10 01:50:29 -0500

    cambios + minimos

--- guias/factura/from_factura.php
+++ guias/factura/from_factura.php
@@ -5,7 +5,7 @@
 			      <div class="modal-content">
 			        <div class="modal-header" style="background-color:#0b5394">
 			          <button type="button" style="color:#FFFFFF" class="close" data-dismiss="modal">&times;</button>
-			          <h4 class="modal-title" style="color:#FFFFFF" align="center"><strong>Factura</strong></h4>
+			          <h4 class="modal-title" style="color:#FFFFFF" align="center"><strong>Generador de Factura</strong></h4>
 			        </div>
 			        <div class="modal-body">
 			          <div>
@@ -52,7 +52,7 @@
 				          </div>
 				          <div class="row">
 			                    <div class="col-md-11 col-sm-12 col-xs-12 form-group">
-			                        <input class='navbar-right btn btn-primary' style="color: #FFF; background-color: #0b5394" size="100" type="submit" value="Generar PDF" name="submit" />			                     
+			                        <input class='navbar-right btn btn-primary' style="color: #FFF; background-color: #0b5394" size="100" type="submit" value="Vista Previa" name="submit" />			                     
 			                    </div>
 			            	</div>
 				        </form> 
