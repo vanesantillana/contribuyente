2c40a1cc82b4a4dff5d160dc0fe3388ab16d19f3
nashvent <anibal.ventura@ucsp.edu.pe>
2017-02-10 22:30:46 -0500

    Aplicaciones añadidas, pendiente 2da app

---
+++ apps/calendario.php
@@ -0,0 +1,14 @@
+<div class="modal fade" id="myModal8" role="dialog">
+	<div class="modal-dialog modal-lg">
+		<div class="modal-content">
+			<div class="modal-header" style="background-color:#0b5394">
+				<button type="button" style="color:#FFFFFF" class="close" data-dismiss="modal">&times;</button>
+				<h4 class="modal-title" style="color:#FFFFFF" align="center"><strong>Calendario</strong></h4>
+			</div>
+			<div class="modal-body">
+				<iframe src="http://www.sunat.gob.pe/orientacion/cronogramas/2017/cObligacionMensual2017.html#ayudaUsuario" width="767" height="1000" scrolling="no"  frameborder="0"></iframe>
+			</div>
+		</div>
+
+	</div>
+</div>
