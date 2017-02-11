71180b7245bfb963881faafcda444fffdd52ed55
vanesantillana <vanessa.santillana@ucsp.edu.pe>
2017-02-11 01:53:03 -0500

    Apps con imagen y calculadora tributaria

--- apps/calc.php
+++ apps/calc.php
@@ -1,12 +1,12 @@
 <div class="modal fade" id="myModal7" role="dialog" >
-	<div class="modal-dialog modal-md">
+	<div class="modal-dialog modal-lg">
 		<div class="modal-content">
 			<div class="modal-header" style="background-color:#0b5394">
 				<button type="button" style="color:#FFFFFF" class="close" data-dismiss="modal">&times;</button>
-				<h4 class="modal-title" style="color:#FFFFFF" align="center"><strong>Calculadora finaciera</strong></h4>
+				<h4 class="modal-title" style="color:#FFFFFF" align="center"><strong>Calculadora Tributaria</strong></h4>
 			</div>
-			<div class="modal-body">
-				<iframe src="http://www.calculatehours.com/Widget/calc.html" width="300" height="250" scrolling="no" frameborder="0"></iframe>
+			<div class="modal-body embed-container">
+				<iframe src="http://www.sunat.gob.pe/cl-at-itcalculibre/actdeuS01Alias#ayudaUsuario" width="770" height="600" scrolling="no" frameborder="0"></iframe>
 			</div>
 		</div>
 
