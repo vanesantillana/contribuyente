9fee56bba9f252f34eea2948b8edc5d47eae08ed
nashvent <anibal.ventura@ucsp.edu.pe>
2017-02-12 12:03:09 -0500

    commit erroneo

--- index.php
+++ index.php
@@ -4,10 +4,11 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <link rel="shortcut icon" href="assets/ico/favicon.ico">
+    <meta name="description" content="Genere documentos de la SUNAT gratis, facturas y acceda a aplicaciones utiles. Herramientas gratis para los contribuyentes peruanos.">
+    <meta name="author" content="elbuencontribuyente.com">
 
+    <link rel="shortcut icon" href="assets/ico/favicon.ico">
+	
     <title>El buen contribuyente</title>
 
     <!-- Bootstrap core CSS -->
