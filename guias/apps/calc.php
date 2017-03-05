c214ba26414c8b075d4c81df119845624876223d
vanesantillana <vanessa.santillana@ucsp.edu.pe>
2017-03-05 02:51:33 -0500

    Pagina actual

---
+++ guias/apps/calc.php
@@ -0,0 +1,14 @@
+<div class="modal fade" id="myModal7" role="dialog" >
+	<div class="modal-dialog modal-lg">
+		<div class="modal-content">
+			<div class="modal-header" style="background-color:#0b5394">
+				<button type="button" style="color:#FFFFFF" class="close" data-dismiss="modal">&times;</button>
+				<h4 class="modal-title" style="color:#FFFFFF" align="center"><strong>Calculadora Tributaria</strong></h4>
+			</div>
+			<div class="modal-body embed-container">
+				<iframe src="http://www.sunat.gob.pe/cl-at-itcalculibre/actdeuS01Alias#ayudaUsuario" width="770" height="600" scrolling="no" frameborder="0"></iframe>
+			</div>
+		</div>
+
+	</div>
+</div>
---
+++ guias/apps/calendario.php
@@ -0,0 +1,16 @@
+<div class="modal fade" id="myModal8" role="dialog">
+	<div class="modal-dialog modal-lg">
+		<div class="modal-content">
+			<div class="modal-header" style="background-color:#0b5394">
+				<button type="button" style="color:#FFFFFF" class="close" data-dismiss="modal">&times;</button>
+				<h4 class="modal-title" style="color:#FFFFFF" align="center"><strong>Calendario</strong></h4>
+			</div>
+			<div class="modal-body">
+				<div class="contenedor" style="width: 100%;">
+				<iframe src="http://www.sunat.gob.pe/orientacion/cronogramas/2017/cObligacionMensual2017.html#ayudaUsuario"  height="1000" scrolling="yes"  frameborder="0"  style="overflow:scroll; max-width: 100%; width: 767px; "></iframe>
+				</div>
+			</div>
+		</div>
+
+	</div>
+</div>
